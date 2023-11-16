7baf4021749061af884bace5053b88e4e0c30777
Track Computer <salmanhabib567@gmail.com>
2023-11-16 13:11:24 +0500

    changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const customerPage = () => {
+  return (
+    <div>Customer page</div>
+  )
+}
+
+export default customerPage
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashBoardPage = () => {
+  return (
+    <div>Dashboard page</div>
+  )
+}
+
+export default dashBoardPage
